c168cc078ba289b9c3ae4ab712829fdfeb063e33
tima <tima430026@mail.ru>
2025-06-16 17:31:42 +0300

    remake styles

--- src/ToDoTask.js
+++ src/ToDoTask.js
@@ -51,27 +51,35 @@ class ToDoTask extends React.Component {
 
   render() {
     return (
-	  <li className="list-group-item">
-				{this.props.task.done ? <div className="todo-indicator bg-success"></div> : <div className="todo-indicator bg-focus"></div>}  
-                    <div className="widget-content p-0">
-                      <div className="widget-content-wrapper">
-                        <div className="widget-content-left">
-                          <div className="widget-heading">{this.props.task.name}</div>
-                          <div className="widget-subheading">
-                            <div>{this.props.task.description}</div>
-                          </div>
-							</div>
-						</div>
-                        <div className="widget-content-right">
-                          <button className="border-0 btn-transition btn btn-outline-success" onClick={this.onStatusClick}>
-                            <i className="fa fa-check"></i>
-							</button>
-                          <button className="border-0 btn-transition btn btn-outline-danger"onClick={this.onDeleteClick}>
-                            <i className="fa fa-trash"></i>
-                          </button>
-                        </div>
-                    </div>
-                  </li>
+	 <li className="list-group-item">
+  {this.props.task.done
+    ? <div className="todo-indicator bg-success"></div>
+    : <div className="todo-indicator bg-focus"></div>}
+  <div className="widget-content p-0 d-flex align-items-center">
+    <div className="widget-content-wrapper">
+      <div className="widget-content-left">
+        <div className="widget-heading">{this.props.task.name}</div>
+        <div className="widget-subheading">
+          <div>{this.props.task.description}</div>
+        </div>
+      </div>
+    </div>
+    <div className="widget-content-right">
+      <button
+        className="border-0 btn-transition btn btn-outline-success"
+        onClick={this.onStatusClick}
+      >
+        <i className="fa fa-check"></i>
+      </button>
+      <button
+        className="border-0 btn-transition btn btn-outline-danger"
+        onClick={this.onDeleteClick}
+      >
+        <i className="fa fa-trash"></i>
+      </button>
+    </div>
+  </div>
+</li>
     );
   }
 }
